02c133a8105e77c8b061f689a58801008b402e0e
lorenmh <neutronstar@gmail.com>
2017-08-18 21:38:26 -0700

    added results

--- trees.py
+++ trees.py
@@ -22,6 +22,7 @@ m_X = m_data.values[:,1:]
 m_X = m_ohe.fit_transform(m_X)
 m_Y = m_data.values[:,0]
 
+print('Mushroom results:')
 for i in range(10):
     m_X_train, m_X_test, m_Y_train, m_Y_test = train_test_split(
             m_X, m_Y, test_size=0.25, random_state=i
@@ -39,10 +40,11 @@ for i in range(10):
     m_gini_acc = accuracy_score(m_gini_predict, m_Y_test)
 
     print(
-        'iteration: %i, m_entropy_acc: %f, m_gini_acc: %f' % (
+        '\titeration: %i, m_entropy_acc: %f, m_gini_acc: %f' % (
             i, m_entropy_acc, m_gini_acc
         )
     )
+
 
 # iris data
 i_data = pd.read_csv('datasets/iris.data', header=None)
@@ -50,6 +52,7 @@ i_data = pd.read_csv('datasets/iris.data', header=None)
 i_X = i_data.values[:,:4]
 i_Y = i_data.values[:,4]
 
+print('\nIris results:')
 for i in range(10):
     i_X_train, i_X_test, i_Y_train, i_Y_test = train_test_split(
             i_X, i_Y, test_size=0.25, random_state=i
@@ -66,7 +69,7 @@ for i in range(10):
     i_gini_acc = accuracy_score(i_gini_predict, i_Y_test)
 
     print(
-        'iteration: %i, i_entropy_acc: %f, i_gini_acc: %f' % (
+        '\titeration: %i, i_entropy_acc: %f, i_gini_acc: %f' % (
             i, i_entropy_acc, i_gini_acc
         )
     )
